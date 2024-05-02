3aef911f1b57edf8bdbeb27734589f75ebef2258
ValentinoChandra <valentino.chandra@itbss.ac.id>
2024-05-02 23:09:39 +0700

    commit

--- index.php
+++ index.php
@@ -35,48 +35,44 @@
         <div style="display: flex; gap: 128px; justify-content: end; margin-right: 4rem;">
             <div style="background-color: #427D9D; border-radius: 16px; padding: 12px; font-size: 16px; text-align: center; margin-top: 50px; ">
                 <h2 style="color: white">Month Expenses</h2>
-                <h2 style="color: white"> 16.000 </h2>
+                <h2 style="color: white"> Rp 16.000 </h2>
             </div>
     
             <div style="background-color: #427D9D; border-radius: 16px; padding: 12px; font-size: 16px; color: white; text-align: center; margin-top: 50px; ">
                 <h2 style="color: white">Month Income</h2>
-                <h2 style="color: white"> 11.000 </h2>
+                <h2 style="color: white"> Rp 11.000 </h2>
             </div>
         </div>
-        <?php $i = 1; ?>
-        <?php while ($row = mysqli_fetch_array($result)) : ?>
-            <div style="display: flex; gap: 32px; justify-content: end; margin-right: 4rem; justify-content: space-between; margin-left: 4rem;">
+            <!-- <div style="display: flex; gap: 32px; justify-content: end; margin-right: 4rem; justify-content: space-between; margin-left: 4rem;">
                 <div style="background-color: #365486; border-radius: 100px; padding: 15px; font-size: 15px; color: white; text-align: center; margin-top: 50px; ">
-                    <h2 style="color: white"><?= $row['day_date']; ?> Date/Day/Time </h2>
+                    <h2 style="color: white"> Date/Day/Time </h2>
                 </div>
                 <div style="display: flex; gap: 8rem;">
                     <div style="background-color: #365486; border-radius: 100px; padding: 15px; font-size: 15px; color: white; text-align: center; margin-top: 50px; ">
-                        <h2 style="color: white"> (+) xxx.xxx </h2>
+                        <h2 style="color: white"> (+) Rp xxx.xxx </h2>
                     </div>
                     <div style="background-color: #365486; border-radius: 100px; padding: 15px; font-size: 15px; color: white; text-align: center; margin-top: 50px; margin-right: 5.6rem">
-                        <h2 style="color: white"> (-) xxx.xxx </h2>
+                        <h2 style="color: white"> (-) Rp xxx.xxx </h2>
                     </div>
                 </div>
-            </div>
-            <div style="background-color: #164863; border-radius: 100px; padding: 20px; margin-top: 20px; margin-left: 2rem; margin-right: 2rem; ">
+            </div> -->
+            <!-- <div style="background-color: #164863; border-radius: 100px; padding: 20px; margin-top: 20px; margin-left: 2rem; margin-right: 2rem; ">
                 <div style="display: flex; gap: 64px; justify-content: end; margin-right: 4rem; justify-content: space-between; margin-left: 4rem;">
                     <div style="display: flex; flex: 1; gap: 64px;">
                         <div style="background-color: #FFF; width: 100%; border-radius: 100px; padding: 15px; font-size: 15px; color: black; text-align: center; margin-top: 0; ">
-                            <h2 style="color: white"> <?= $row['item_name']; ?> Item Name</h2>
+                            <h2 style="color: white"> Item Name</h2>
                         </div>
                         <div style="background-color: #FFF; width: 100%; border-radius: 100px; padding: 15px; font-size: 15px; color: black; text-align: center; margin-top: 0; ">
-                            <h2 style="color: white"> <?= $row['note']; ?> Note </h2>
+                            <h2 style="color: white"> Note </h2>
                         </div>
                     </div>
                     <div style="background-color: #FFF; width: 100%; border-radius: 100px; padding: 15px; font-size: 15px; color: black; text-align: center; margin-top: 0; flex: 1; display: flex; justify-content: space-around">
-                        <!-- <h2> (-) xxx.xxx </h2> -->
+                         <h2> (-) xxx.xxx </h2>
                         <h2></h2>
-                        <h2 style="color: white"> <?= $row['total price']; ?> (-) xxx.xxx </h2>
+                        <h2 style="color: white"> (-) Rp xxx.xxx </h2>
                     </div>
                 </div>
-            </div>
-        <?php $i++; ?>
-        <?php endwhile; ?>
+            </div> -->
 
             <div style="display: flex; gap: 32px; justify-content: end; margin-right: 4rem; justify-content: space-between; margin-left: 4rem;">
                 <div style="background-color: #365486; border-radius: 100px; padding: 15px; font-size: 15px; color: white; text-align: center; margin-top: 20px; ">
@@ -84,10 +80,10 @@
                 </div>
                 <div style="display: flex; gap: 8rem;">
                     <div style="background-color: #365486; border-radius: 100px; padding: 15px; font-size: 15px; color: white; text-align: center; margin-top: 20px; ">
-                        <h2 style="color: white"> + 0 </h2>
+                        <h2 style="color: white"> + Rp 0 </h2>
                     </div>
                     <div style="background-color: #365486; border-radius: 100px; padding: 15px; font-size: 15px; color: white; text-align: center; margin-top: 20px; margin-right: 5.6rem">
-                        <h2 style="color: white"> - 11.000 </h2>
+                        <h2 style="color: white"> - Rp 11.000 </h2>
                     </div>
                 </div>
             </div>
@@ -104,7 +100,7 @@
                     <div style="background-color: #FFF; width: 100%; border-radius: 100px; padding: 15px; font-size: 15px; color: black; text-align: center; margin-top: 0; flex: 1; display: flex; justify-content: space-around">
                         <!-- <h2> (-) xxx.xxx </h2> -->
                         <h2></h2>
-                        <h2> - 11.000 </h2>
+                        <h2> - Rp 11.000 </h2>
                     </div>
                 </div>
             </div>
@@ -115,10 +111,10 @@
                 </div>
                 <div style="display: flex; gap: 8rem;">
                     <div style="background-color: #365486; border-radius: 100px; padding: 15px; font-size: 15px; color: white; text-align: center; margin-top: 20px; ">
-                        <h2 style="color: white"> + 0 </h2>
+                        <h2 style="color: white"> + Rp 0 </h2>
                     </div>
                     <div style="background-color: #365486; border-radius: 100px; padding: 15px; font-size: 15px; color: white; text-align: center; margin-top: 20px; margin-right: 5.6rem">
-                        <h2 style="color: white"> - 5.000 </h2>
+                        <h2 style="color: white"> - Rp 5.000 </h2>
                     </div>
                 </div>
             </div>
@@ -135,7 +131,7 @@
                     <div style="background-color: #FFF; width: 100%; border-radius: 100px; padding: 15px; font-size: 15px; color: black; text-align: center; margin-top: 0; flex: 1; display: flex; justify-content: space-around">
                         <!-- <h2> (-) xxx.xxx </h2> -->
                         <h2></h2>
-                        <h2> - 5.000 </h2>
+                        <h2> - Rp 5.000 </h2>
                     </div>
                 </div>
             </div>
@@ -146,10 +142,10 @@
                 </div>
                 <div style="display: flex; gap: 8rem;">
                     <div style="background-color: #365486; border-radius: 100px; padding: 15px; font-size: 15px; color: white; text-align: center; margin-top: 20px; ">
-                        <h2 style="color: white"> + 11.000 </h2>
+                        <h2 style="color: white"> + Rp 11.000 </h2>
                     </div>
                     <div style="background-color: #365486; border-radius: 100px; padding: 15px; font-size: 15px; color: white; text-align: center; margin-top: 20px; margin-right: 5.6rem">
-                        <h2 style="color: white"> - 0 </h2>
+                        <h2 style="color: white"> - Rp 0 </h2>
                     </div>
                 </div>
             </div>
@@ -164,7 +160,7 @@
                         </div>
                     </div>
                     <div style="background-color: #FFF; width: 100%; border-radius: 100px; padding: 15px; font-size: 15px; color: black; text-align: center; margin-top: 0; flex: 1; display: flex; justify-content: space-around">
-                        <h2> + 11.000 </h2>
+                        <h2> + Rp 11.000 </h2>
                         <h2></h2>
                         <!-- <h2> (-) xxx.xxx </h2> -->
                     </div>
